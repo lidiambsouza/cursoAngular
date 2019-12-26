8599fddcec1735888811abf7b1fbe7a39a884e01
lidiambsouza <lidiambsouza@gmail.com>
2019-12-26 14:04:56 -0300

    criando modulo

--- jogoDaVelha/src/app/app.component.spec.ts
+++ jogoDaVelha/src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {JogoDaVelhaModule} from "./jogo-da-velha";
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        JogoDaVelhaModule
       ],
       declarations: [
         AppComponent
@@ -20,16 +22,5 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'jogoDaVelha'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('jogoDaVelha');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('jogoDaVelha app is running!');
-  });
+  
 });
